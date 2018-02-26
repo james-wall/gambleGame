8b8b7e74a5a42d4e4196067450e41925b803478a
James <james.wall@wustl.edu>
2018-02-25 18:31:29 -0800

    got the try/catch exceptions figured out

--- gamble.py
+++ gamble.py
@@ -5,50 +5,90 @@
 import tkinter
 #...and for picking random names.
 import random
+try:
+	#a function will determine the results of the game
+	def playGame(riskLevel):
+		if riskLevel > 10 or riskLevel < 1:
+			print('Sorry champ, try again. You need to give me a risk level between 1 and 10 (inclusive)')
+			return
 
-#a function will determine the results of the game
-def playGame(riskLevel):
-	# todo add logic for keeping between 1 and 10
+		personOne = random.choice(names) #tuple1
+		#names.remove(personOne) #prevents double picking
+		personTwo = random.choice(names) #tuple2
 
-	personOne = random.choice(names) #tuple1
-	#names.remove(personOne) #prevents double picking
-	personTwo = random.choice(names) #tuple2
+		moneyOwed = riskLevel/10 * personOne[1]
+		moneyOwedString = "%.2f" % moneyOwed
 
-	moneyOwed = riskLevel/10 * personOne[1]
-	moneyOwedString = "%.2f" % moneyOwed
+		message = personTwo[0] + ' won! ' + personOne[0] + ' lost :/ ... ' + personOne[0] +' owes ' + personTwo[0] + ' $' + moneyOwedString + '!'
+		print(message)
+		#nameLabel.configure(text=message)
 
-	message = personTwo[0] + ' won! ' + personOne[0] + ' lost :/ ... ' + personOne[0] +' owes ' + personTwo[0] + ' $' + moneyOwedString + '!'
-	print(message)
-	#nameLabel.configure(text=message)
+	print('\n\nOh hai there')
+	print('Welcome to the world-renowned gambling emporium festivus:')
+	print('The Gambling Game!\n')
+	print('(Press Ctrl-C at any time to exit the game)\n\n')
 
-print('Oh hai there')
-print('Welcome to the world-renowned gambling emporium festivus:')
-print('The Gambling Game!\n\n')
+	num = 0 
+	tryAgain = False;
+	while True:
+		try:
+			# Get number of people participating
+			num = int(input('So how many people do we have participating with us on this fine day?\n')) # todo add try catch for invalid input and negative input
+			if num < 2:
+				print('There needs to be at least two people in order to play the game!')
+				pass
+			break
+		except (ValueError, TypeError):
+			print('You need to give me at least two people\'s names to play the game!')
+			print('Please try again\n')
+			pass
+		print('Input is invalid, please try again')
 
-# Get number of people participating
-num = int(input('So how many people do we have participating with us on this fine day?\n')) # todo add try catch for invalid input and negative input
+	print('Fantastic!')
 
-print('Fantastic!')
+	#the list of possible names.
+	names = []
 
-#the list of possible names.
-names = []
+	for i in range (0, num):
+		while True:
+			try:
+				personNum = i + 1
+				personName = str(input('Who\'s person number ' + str(personNum) + '?\n')) # todo add try/catch
+				print('Worddddd\n')
+				personMoney = int(input('So how much money does ' + personName + ' have with them?\n'))
+				# todo add joke based on money amount
+				print('Let\'s here it for Contestant #' + str(personNum) + ', ' + personName + '!\n\n\n')
+				personTuple = (personName, personMoney)
+				names.append(personTuple)
+				break           
+			except (ValueError, TypeError):
+				print('Please give a name and a money value.')   
+				pass
+			print('\nIncorrect type of input, please try again')
 
-for i in range (0, num):
-	personNum = i + 1
-	personName = str(input('Who\'s person number ' + str(personNum) + '?')) # todo add try/catch
-	print('Worddddd\n')
-	personMoney = int(input('So how much money does ' + personName + ' have with them?\n'))
-	# todo add joke based on money amount
-	print('Let\'s here it for Contestant #' + str(personNum) + ', ' + personName + '!\n\n\n')
-	personTuple = (personName, personMoney)
-	names.append(personTuple)
+	try:
+		if len(names) < 2:
+			raise  ValueError('You need to give me at least two people\'s names to play the game!')
+	except ValueError as error:
+		print(str(error))
 
-if len(names) < 1:
-	throw #add throw logic
+	while True:
+		try:
+			riskLevel = int(input("Ok, scale of 1 to 10, 10 being the highest, how much risk do we want to take on today?\n"))
+			if riskLevel > 10 or riskLevel < 1:
+				print('Sorry champ, try again. You need to give me a risk level between 1 and 10 (inclusive)')
+				pass
+			break
+		except (ValueError, TypeError):
+			print('Invalid input, please try again.')
+			pass
+		print ('\nIncorrect type of input, please try again')
+	
+	playGame(riskLevel)
 
-riskLevel = int(input("Ok, scale of 1 to 10, 10 being the highest, how much rish do we want to take on today?\n"))
-
-playGame(riskLevel)
+except (KeyboardInterrupt):
+	print('Thanks for playing! See you next time on... ')
+	print('\t\t\t\t\t\t\t...The Gambling Game!')
 
 
 # #create a GUI window.
